4ae02f9e768d9e8541a76223b311c2cdb90e9ddc
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-02-10 11:25:25 +0100

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,10 @@
+
+/*** Rechner */
+/*
+0. a+b / a-b/ a*b / a/b  // ergebnis c
+1. Dateneingabe + -überprüfung :: check
+2. Auswahl Rechenart :: check
+3. Fkt. Grundrechenarten :: check
+4. Ausgabe in Konsole :: check
+*/
+
