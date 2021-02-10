46f642e3e72694d9076b683dea175b0d2f912b1c
andante-maestoso <h.plottke@mail.de>
2021-02-10 14:37:30 +0100

    Modul 3

--- script_01.js
+++ script_01.js
@@ -7,13 +7,42 @@
 3. Fkt. Grundrechenarten :: 
 4. Ausgabe in Konsole :: check
 */
+let a,b,c
+a=6
+b=2
+//Modul 1: Eingabe und Überprüfung
 
+//Modul 2: Auswahl Rechenart 
 
+//Modul 3: Grundrechenarten
 
-//Modul 3: 
+//Addition
+function addition(a,b) {
+    c = a + b;
+    return c;
+}
+
+//Subtraktion
+function subtraktion(a,b) {
+    c = a - b;
+    return c;
+}
+
+//Multiplikation
+function multiplikation(a,b) {
+    c = a * b;
+    return c;
+}
+
+//Division
+function division(a,b) {
+    c = a / b;
+    return c;
+}
 
 //Modul 4: Konsolenausgabe | Test:
 //ausgabe ("hi");
+//ausgabe (division(a,b));
 function ausgabe(outputStr) {
     console.log(outputStr);
 }
