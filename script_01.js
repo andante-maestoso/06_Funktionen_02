e4519e413abf867a5175cf413a1ddd5909ed504a
andante-maestoso <h.plottke@mail.de>
2021-02-10 15:04:46 +0100

    Modul 2

--- script_01.js
+++ script_01.js
@@ -7,37 +7,58 @@
 3. Fkt. Grundrechenarten :: 
 4. Ausgabe in Konsole :: check
 */
-let a,b,c
+let a,b,c, rechenart
 a=6
 b=2
+rechenart = "mal"
+ 
 //Modul 1: Eingabe und Überprüfung
 
-//Modul 2: Auswahl Rechenart 
 
+//Modul 2: Auswahl Rechenart 
+//function auswahl(rechenart, a, b){
+    switch (rechenart) {
+        case rechenart = "plus":
+            ausgabe (addition(a,b));
+            break;
+
+        case rechenart = "minus":
+            ausgabe (subtraktion(a,b));
+        break;
+
+        case rechenart = "mal":
+            ausgabe (multiplikation(a,b));
+            break;
+
+        case rechenart = "geteilt":
+            ausgabe (division(a,b));
+            break; 
+
+        default:
+            console.log("Keine gültige Rechenart!")
+            break;
+    }
+//}
 //Modul 3: Grundrechenarten
 
 //Addition
 function addition(a,b) {
-    c = a + b;
-    return c;
+    return a + b;
 }
 
 //Subtraktion
 function subtraktion(a,b) {
-    c = a - b;
-    return c;
+    return a - b;
 }
 
 //Multiplikation
 function multiplikation(a,b) {
-    c = a * b;
-    return c;
+    return a * b;
 }
 
 //Division
 function division(a,b) {
-    c = a / b;
-    return c;
+    return a / b;
 }
 
 //Modul 4: Konsolenausgabe | Test:
